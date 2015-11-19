92db8ca151cc504ade443dddc81d736f62325daa
8108_web (Teacher) <t_8108_web@PEDAGOGIQUE.LAN>
2015-11-19 14:28:43 +0100

    Insert exercice 3

---
+++ ex3/index.js
@@ -0,0 +1,30 @@
+// //Base
+// var fs = require("fs");
+// fs.readdir('fichiers',  function(err, data) {
+//   if (err) throw err;
+//   console.log("There is "+data.length+" files in this folder, they are :")
+//
+//   for (var i = 0,l = data.length; i < l; i++) {
+//     console.log(data[i]);
+//   }
+// });
+
+// //ES5
+// var fs = require("fs");
+// fs.readdir('fichiers',  function(err, data) {
+//   if (err) throw err;
+  console.log("There is "+data.length+" files in this folder, they are :")
+//   data.forEach(function(elem) {
+//     console.log(elem);
+//   });
+// });
+// ES6
+// "use strict";
+// let fs = require("fs");
+// fs.readdir('fichiers',  (err, data)=> {
+//   if (err) throw err;
+  // console.log(`There is ${data.length} files in this folder, they are :`);
+//   data.forEach(elem => {
+//     console.log(elem)
+//   });
+// });
