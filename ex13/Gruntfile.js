ff428a1740920f982460683b8ae3c200be90b2bc
8108_web (Teacher) <t_8108_web@PEDAGOGIQUE.LAN>
2015-12-04 16:30:18 +0100

    site generator

--- ex13/Gruntfile.js
+++ ex13/Gruntfile.js
@@ -15,6 +15,19 @@ module.exports = function(grunt) {
         }
       }
     },
+    handlebarslayouts: {
+      dist: {
+        files: {
+          'dist/*.html': 'src/hbs/pages/*.hbs'
+        },
+        options: {
+          partials: [
+            'src/hbs/partials/*.hbs'
+          ],
+          context: "src/hbs/datas.json"
+        }
+      }
+    },
     copy: {
       dist: {
         files: [{
@@ -25,6 +38,27 @@ module.exports = function(grunt) {
         }],
       },
     },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src/sass',
+          cssDir: 'dist/css',
+          environment: 'production'
+        }
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['img/**/*.{png,jpg,gif}'],
+          dest: '<%= files.destination %>'
+        }]
+      }
+    },
+    clean: ["dist"],
+
     connect: {
       server: {
         options: {
@@ -32,6 +66,19 @@ module.exports = function(grunt) {
           hostname: '*',
           base: '<%= files.destination %>'
         }
+      }
+    },
+    ftpush: {
+      prod: {
+        auth: {
+          host: 'dev.technocite.lan',
+          port: 21,
+          authKey: 'key1'
+        },
+        src: 'dist/',
+        dest: '/',
+        simple: false,
+        useList: false
       }
     },
     watch: {
@@ -42,9 +89,13 @@ module.exports = function(grunt) {
         files: '<%= files.js %>',
         tasks: ['uglify:dist']
       },
-      html: {
-        files: 'src/*.html',
-        tasks: ['copy:dist']
+      sass: {
+        files: 'src/sass/**/*.scss',
+        tasks: ['compass:dist']
+      },
+      hbs: {
+        files: ['src/hbs/**/**/*.hbs', 'src/hbs/**/**/*.json'],
+        tasks: ['handlebarslayouts']
       }
     },
   });
@@ -54,6 +105,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks("grunt-handlebars-layouts");
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-ftpush');
   // Default task(s).
-  grunt.registerTask('default', ['uglify:dist', 'copy:dist', 'connect', 'watch']);
+  grunt.registerTask('default', ['uglify:dist', 'handlebarslayouts', 'compass', 'connect', 'watch']);
+  grunt.registerTask('push2prod', ['clean','uglify:dist', 'handlebarslayouts', 'compass',"ftpush:prod"]);
 };
